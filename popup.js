9cee5c935b493610702eb18e9086209da57fcdd1
Ben <ben_hubbard@hotmail.co.uk>
2017-11-04 18:44:11 +0000

    added speak ability

--- popup.js
+++ popup.js
@@ -23,5 +23,6 @@ function runReplaceImage() {
 document.addEventListener('DOMContentLoaded', () => {
     getCurrentTabUrl((url) => {
         runReplaceImage();
+        chrome.tts.speak('Hi, Jordan, Welcome to this shit.');
     });
 });
