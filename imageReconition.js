02992b0939726ebf20e9934e3f9d9448c798f94c
vixvpr <vprking1@gmail.com>
2017-11-04 15:39:11 +0000

    Made a working request for img rec

--- imageReconition.js
+++ imageReconition.js
@@ -1,17 +1,21 @@
-var watson = require('watson-developer-cloud');
-var visual_recgnition = watson.visual_recognition({
-    api_key: '{api-key}',
-    version_date: '2016-05-20'
-});
 //Expects the url of the image
-function convertImageToText(imageURLToString){
-    return "Yeah you called right";
+function convertImageToText(imageURLToString) {
+    var watson = require('watson-developer-cloud');
+    var visual_recgnition = watson.visual_recognition({
+        api_key: '{api-key}',
+        version_date: '2016-05-20'
+    });
     var params = {
-        image_file : fs.createReadStream(imageURLToString)
+        image_file: fs.createReadStream(imageURLToString)
     };
-
+    visual_recgnition.classify(params, function (err, res) {
+        if (err)
+            console.log(err)
+        else
+            return parseReturnedValue(JSON.stringify(res, null, 2));
+    });
 }
 
-function parseReturnedValue(jsonReturned){
-
+function parseReturnedValue(jsonReturned) {
+    console.log(jsonReturned);
 }
