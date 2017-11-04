3c49d64ab3dd059ac50cb348c7c0e62cb52403d0
Gordon Mason <nqb15168@uni.strath.ac.uk>
2017-11-04 15:52:29 +0000

    #RememberRemember to pull

--- imageReconition.js
+++ imageReconition.js
@@ -1,10 +1,16 @@
 //Expects the url of the image
+
+var fs = require('fs');
+
+
+var watson = require('watson-developer-cloud');
+var visual_recgnition = watson.visual_recognition({
+    api_key: '{JV6fK0YispGmC8qKZo0FyNZGZKrq7olmNJ-LEeRO95cQ}',
+    version: 'v3',
+    version_date: '2016-05-20'
+});
+
 function convertImageToText(imageURLToString) {
-    var watson = require('watson-developer-cloud');
-    var visual_recgnition = watson.visual_recognition({
-        api_key: '{api-key}',
-        version_date: '2016-05-20'
-    });
     var params = {
         image_file: fs.createReadStream(imageURLToString)
     };
@@ -19,3 +25,6 @@ function convertImageToText(imageURLToString) {
 function parseReturnedValue(jsonReturned) {
     console.log(jsonReturned);
 }
+
+
+convertImageToText('./cat.jpeg');
