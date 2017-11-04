3d8898e488368d83d56ffdeffd392ded942e6ab4
Gordon Mason <nqb15168@uni.strath.ac.uk>
2017-11-04 15:21:32 +0000

    #FirstEver imported watson

--- imageReconition.js
+++ imageReconition.js
@@ -1,4 +1,8 @@
-
+var watson = require('watson-developer-cloud');
+var visual_recgnition = watson.visual_recognition({
+    api_key: '{api-key}',
+    version_date: '2016-05-20'
+});
 //Expects the url of the image
 function convertImageToText(imageURLToString){
     return "Yeah you called right";
