4c75e16ea74f412d33c13cb1a8018fff5b607a90
Jordan Scott Howorth CS2015 <jordan.howorth.2015@uni.strath.ac.uk>
2017-11-04 17:44:06 +0000

    #ThisAgain Hack for removing ALL the images.

--- replaceImage.js
+++ replaceImage.js
@@ -1,9 +1,22 @@
-var x = document.getElementsByTagName("p");
+y();
 
-for (var i = 0; i < x.length; i++) {
-    var plaint = document.createElement("p");
-    plaint.innerText =  "Greg";
+function y(){
+    while(x()>0);
+}
+function x() {
+    var x = document.getElementsByTagName("img");
+    var newSpan = document.createElement("span");
+    newSpan.innerHTML = "blaaaaaaaaaaah";
+    var body = document.getElementsByTagName("body");
 
-    x.item(i).parentNode.replaceChild(plaint, x.item(i));
-    console.log(i);
+    for (var i = 0; i < x.length; i++) {
+        var plaint = document.createElement("p");
+        plaint.innerHTML = "Greg";
+
+        console.log(x.item(i).innerHTML);
+        x.item(i).parentNode.replaceChild(plaint, x.item(i));
+        console.log(i);
+    }
+    console.log("run finished");
+    return x.length;
 }
