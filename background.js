4e56e1d3be5edf4ed6eec3392946cdf41a7c229d
Niklavs Meiers <niklavs@burti.lv>
2017-11-04 21:26:15 +0000

    Added timer for Description

--- background.js
+++ background.js
@@ -1,9 +1,16 @@
+
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        chrome.tts.speak("Art expo");
-        chrome.tabs.executeScript({
+    function (request, sender, sendResponse) {
+        setTimeout(function () {
+            chrome.tts.speak("This is a beautiful art page")
+        }, 10000);
+        setTimeout(function () {
+            chrome.tabs.executeScript({
                 file: "replaceImage.js"
             });
+        }, 12000);
+        //chrome.tts.speak("Art expooooooooooooooo");
+
         console.log(sender.tab ?
             "from a content script:" + sender.tab.url :
             "from the extension");
