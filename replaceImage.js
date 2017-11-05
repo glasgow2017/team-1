f9bbe58ae69cbf82fa29402eef63032d83f66acc
vixvpr <vprking1@gmail.com>
2017-11-05 00:19:09 +0000

    added some cats and made it return the string needed for display

--- replaceImage.js
+++ replaceImage.js
@@ -2,18 +2,17 @@
 function findContext(url4u){
 
     var watson_server = new XMLHttpRequest();
-
+    var stringRep = "";
     watson_server.open("POST", "http://localhost:8080/image", true);
     watson_server.setRequestHeader("Content-type","text/plain");
-
     watson_server.onreadystatechange = function(x){
         if(watson_server.readyState == XMLHttpRequest.DONE && watson_server.status == 200){
-            console.log(watson_server.responseText);
-             
+            stringRep = (watson_server.responseText);
         }
-    }
+    };
 
     watson_server.send(url4u);
+    return stringRep;
 }
 //For some reason, all images are retrieved but they're null after the halfway point. Please ignore this hack.
 single_run();
@@ -26,8 +25,7 @@ function single_run() {
     while(x.length>0) {
             var plaint = document.createElement("p");
             console.log(x.item(0).src);
-            plaint.innerHTML = findContext(x.item(0).src);
-
+            plaint.innerHTML =  findContext(x.item(0).src);
             x.item(0).parentNode.replaceChild(plaint, x.item(0));
          }
      return x.length;
