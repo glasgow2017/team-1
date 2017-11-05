75f5ac8154b654ac58b866583dabd4cedae04b5c
Niklavs Meiers <niklavs@burti.lv>
2017-11-05 08:23:24 +0000

    Added description output

--- background.js
+++ background.js
@@ -1,10 +1,14 @@
+
+
+
 chrome.runtime.onMessage.addListener(
 
     function (request, sender, sendResponse) {
 
-        var assoc = {"instagram":"Instagram, welcome ","pinterest":"Pinterest, welcome2", "artweb":"Art Web, this is art"};
+
+        var assoc = {"instagram":"Instagram, welcome ","pinterest":"Pinterest, welcome2", "artweb":"Painters online is an online gallery for art lovers"};
         setTimeout(function () {
             chrome.tts.speak(assoc[request.greeting]);
-        }, 1);
+        }, 10000);
             sendResponse({farewell: assoc[request.greeting]});
     });
