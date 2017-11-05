af3a430bd6df864da0eb6e0ef6a69ab02a256fba
Ben <ben_hubbard@hotmail.co.uk>
2017-11-05 03:34:13 +0000

    #RememberRemember merge

--- background.js
+++ background.js
@@ -1,24 +1,10 @@
-
-
-
 chrome.runtime.onMessage.addListener(
 
     function (request, sender, sendResponse) {
 
-
         var assoc = {"instagram":"Instagram, welcome ","pinterest":"Pinterest, welcome2", "artweb":"Art Web, this is art"};
         setTimeout(function () {
-            chrome.tts.speak(assoc[request.greeting])
-        }, 10000);
-        setTimeout(function () {
-            chrome.tabs.executeScript({
-                file: "replaceImage.js"
-            });
-        }, 12000);
-        //chrome.tts.speak("Art expooooooooooooooo");
-        //
-        // console.log(sender.tab ?
-        //     "from a content script:" + sender.tab.url :
-        //     "from the extension");
+            chrome.tts.speak(assoc[request.greeting]);
+        }, 1);
             sendResponse({farewell: assoc[request.greeting]});
     });
--- popup.js
+++ popup.js
@@ -19,6 +19,7 @@ function runReplaceImage() {
     });
 }
 
+runReplaceImage();
 
 document.addEventListener('DOMContentLoaded', () => {
     getCurrentTabUrl((url) => {
